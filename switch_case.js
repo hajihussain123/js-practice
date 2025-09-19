316a4527c9e2842214243f0fdce0f8fd432602e0
hajihussain123 <hajihussainmohammad@gmail.com>
2025-09-19 17:26:57 +0530

    Create javascript program to demonstrate switch case

---
+++ switch_case.js
@@ -0,0 +1,23 @@
+let movieToWatch = "avengers endgame";
+
+switch(movieToWatch){
+  case "coolie":
+    console.log("you are going to watch coolie");
+    break;
+
+  case "game changer":
+    console.log("you are watching game changer");
+    break;
+
+  case "godfather":
+    console.log("you are watching godfather");
+    break;
+
+  case "avengers endgame":
+    console.log("you are watching avengers endgame");
+    break;
+    
+  default:
+    console.log("you are watching nothing.. goood boy");
+    break;
+}
