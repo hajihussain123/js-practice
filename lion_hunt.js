5498bfecd817597c91dd605f416f3b4d91895b74
hajihussain123 <hajihussainmohammad@gmail.com>
2025-09-20 19:51:51 +0530

    Update  to find distance in test case 1

--- lion_hunt.js
+++ lion_hunt.js
@@ -1,3 +1,17 @@
-const lionZebraLocation = "";
-const distanceOfPath = -1;
+const lionZebraLocation = "LZ";
+const lion = "L";
+const zebra = "Z";
+let distanceOfPath = -1;
+
+let distanceCounter = 0;
+let counter = 0;
+if (lionZebraLocation[0] === lion){
+  if(lionZebraLocation[1] !== zebra){
+    counter++;
+    distanceCounter++;
+  }
+}
+
+distanceOfPath = distanceCounter;
+
 console.log("the shortest distance is",distanceOfPath);
