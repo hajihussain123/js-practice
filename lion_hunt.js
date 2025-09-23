c837f44e02a20463d668b527fefa0ecccc2b8f91
hajihussain123 <hajihussainmohammad@gmail.com>
2025-09-23 09:25:38 +0530

    Update to find shortest path

--- lion_hunt.js
+++ lion_hunt.js
@@ -1,17 +1,58 @@
-const lionZebraLocation = "LZ";
+const lionZebraLocation = "Z L ZL Z L";
 const lion = "L";
 const zebra = "Z";
 let distanceOfPath = -1;
 
-let distanceCounter = 0;
 let counter = 0;
-if (lionZebraLocation[0] === lion){
-  if(lionZebraLocation[1] !== zebra){
-    counter++;
-    distanceCounter++;
+let minimumDistance = -1;
+
+let counterForward;
+for(counter = 0; counter < lionZebraLocation.length - 1; counter++){
+
+  if(lionZebraLocation[counter] === lion){
+
+    counterForward = counter + 1;
+    while(counterForward < lionZebraLocation.length && lionZebraLocation[counterForward] !== zebra){
+      counterForward++;
+    }
+
+    if(counterForward !== lionZebraLocation.length){
+      counterForward = counterForward - counter - 1;
+  
+      if(minimumDistance === -1){
+        minimumDistance = counterForward;
+      } else{
+        minimumDistance = minimumDistance > counterForward ? counterForward : minimumDistance; 
+      }
+    }
+
   }
 }
 
-distanceOfPath = distanceCounter;
+
+let counterBackward;
+for(counter = lionZebraLocation.length - 1; counter > 0; counter--){
+
+  if(lionZebraLocation[counter] === lion){
+
+    counterBackward = counter - 1;
+    while(counterBackward >= 0 && lionZebraLocation[counterBackward] !== zebra){
+      counterBackward--;
+    }
+
+    if(counterBackward !== -1){
+      counterBackward = counter - counterBackward - 1;
+  
+      if(minimumDistance === -1){
+        minimumDistance = counterBackward;
+      } else{
+        minimumDistance = minimumDistance > counterBackward ? counterBackward : minimumDistance; 
+      }
+    }
+
+  }
+}
+
+distanceOfPath = minimumDistance;
 
 console.log("the shortest distance is",distanceOfPath);
