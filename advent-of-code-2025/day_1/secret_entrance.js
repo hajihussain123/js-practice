656eb43d898da506c1920f4eb796cd2e802315a8
hajihussain123 <hajihussainmohammad@gmail.com>
2025-12-13 12:24:35 +0530

    Create secret_entrance.js

---
+++ advent-of-code-2025/day_1/secret_entrance.js
@@ -0,0 +1,56 @@
+const parseInput = (input) => {
+  return input.split("\n");
+};
+
+const rotateRight = (dial, steps) => {
+  let countOfZeroes = 0;
+  for (let counter = 1; counter <= steps; counter++) {
+    countOfZeroes += dial.heading === 0 ? 1 : 0;
+    dial.heading = (dial.heading + 1) % 100;
+  }
+  return countOfZeroes;
+};
+
+const rotateLeft = (dial, steps) => {
+  let countOfZeroes = 0;
+  for (let counter = 1; counter <= steps; counter++) {
+    countOfZeroes += dial.heading === 0 ? 1 : 0;
+    dial.heading = dial.heading - 1 < 0 ? 99 : dial.heading - 1;
+  }
+  return countOfZeroes;
+};
+
+const rotations = {
+  "R": rotateRight,
+  "L": rotateLeft,
+};
+
+const execute = (dial, instruction) => {
+  const countOfZeroes = rotations[instruction[0]](dial, +instruction.slice(1)); // instruction ex:- "R56"
+  return [countOfZeroes, dial.heading];
+};
+
+const rotate = (dial, instructions) => {
+  const headings = instructions.map((command) => execute(dial, command));
+  return headings;
+};
+
+const part1 = (dial, input) => {
+  const header = { ...dial };
+  const instructions = parseInput(input);
+  const headings = rotate(header, instructions);
+  return headings;
+};
+
+const part2 = (dial, instructions) => {
+  const header = { ...dial };
+  return part1(header, instructions).reduce(
+    (total, [countOfZeroes]) => total + countOfZeroes,
+    0,
+  );
+};
+
+const dial = { heading: 50 };
+const puzzleInput = Deno.readTextFileSync("./day_1_input.txt");
+console.log(part1(dial, puzzleInput).filter((value) => value[1] === 0).length);
+console.log(part2(dial, puzzleInput));
